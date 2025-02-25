68f473f81b6cd1f7d6dfa900656c8fc3f7e5b780
Ahmed Khalid <sf.ahmed.uv@gmail.com>
2025-02-25 10:56:56 +0400

    pool

--- Fibonacci.py
+++ Fibonacci.py
@@ -22,3 +22,4 @@ n = eval(input("\nPlease enter the nth term you would like from the Fibonacci se
 print(f"\nThe {n}th term of the Fibonacci sequence is:\n{fib(n)}")
 
 print("I Will Change")
+print("YEPPIE")
